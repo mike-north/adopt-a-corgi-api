12982680d76985060ba6ab53b2e3718d656ecf3d
Michael North <michael.l.north@gmail.com>
2017-11-22 17:31:10 -1000

    puppy data

--- index.js
+++ index.js
@@ -2,10 +2,39 @@
 const express = require('express');
 let app = express();
 
+const PUPPY_DATA = [
+  {
+    name: "Fred",
+    breed: "Pembroke Welsh Corgi",
+    birthday: "10/27/2017",
+    vaccinated: true,
+    gender: "male"
+  },
+  {
+    name: "Babe",
+    breed: "Pembroke Welsh Corgi",
+    birthday: "10/18/2017",
+    vaccinated: true,
+    gender: "female"
+  },
+  {
+    name: "Sire",
+    breed: "Pembroke Welsh Corgi",
+    birthday: "10/18/2012",
+    vaccinated: true,
+    gender: "female"
+  },
+  {
+    name: "Biscuit",
+    breed: "Pembroke Welsh Corgi",
+    birthday: "10/01/2017",
+    vaccinated: true,
+    gender: "male"
+  }
+];
+
 app.get('/favorites', (req, res) => {
-  res.json([
-    {name: 'PUPPY!'}
-  ])
+  res.json(PUPPY_DATA);
 });
 
 app.listen(process.env.PORT || 3000);
